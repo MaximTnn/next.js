28e00d9bc5fddacf639e458ce9c9a64932b2a081
Maxim Tyumenbaev <tyumenbaev.m@gmail.com>
2024-03-25 22:27:28 +0300

    add optional universal segments

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -7,12 +7,13 @@ export default function Docs({params}: {
 }) {
     console.log(params)
 
-    if(params.slug.length === 2) {
+    if(params.slug?.length === 2) {
         return <h1>Viewing docs for feature {params.slug[0]} and concept {params.slug[1]}</h1>
-    } else if (params.slug.length === 1) {
+    } else if (params.slug?.length === 1) {
         return <h1>Viewing docs for feature {params.slug[0]}</h1>
     }
     return (
         <h1>Docs hame page</h1>
     );
 }
+
