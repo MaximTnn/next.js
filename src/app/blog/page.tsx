aa0c12dd2f653401612c38e23dec5d2c07147fc1
Maxim Tyumenbaev <tyumenbaev.m@gmail.com>
2024-04-01 23:08:06 +0300

    title Metadata has been studied

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,12 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata ={
+    title: {
+        // absolute игнорирует изменение title через родительский title template в layout
+        absolute: 'Blog',
+    }
+}
+
 export default function Blog() {
     return <h1>My blog</h1>
 }
