74a60b49acaab0270c6cdc8b28f5b59405c29fcb
Maxim Tyumenbaev <tyumenbaev.m@gmail.com>
2024-06-03 22:41:19 +0300

    Recovering from Errors has been studied

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,10 +1,12 @@
 'use client'
 
-// В Error.message мы отправиди сообщение об ошибке из компонента
-export default function ErrorBoundary({ error }: { error: Error }) {
+// В Error.message мы отправили сообщение об ошибке из компонента
+export default function ErrorBoundary({ error, reset }: { error: Error, reset: () => void }) {
     console.log(error.message)
+    // reset - функция для перезагрузки компонента
 
     return <div>
         Error message: {error.message}
+        <button onClick={reset}>Try again</button>
     </div>
 }
