368c7f1e896a364554aeb37f1193b1e3ea67abef
Maxim Tyumenbaev <tyumenbaev.m@gmail.com>
2024-06-24 22:53:23 +0300

    Intercepting Routes has been learned

---
+++ src/app/f1/f4/(..)f3/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function InterceptedF3() {
+    return (
+        <h1>
+            (..) Intercepted F3 page
+        </h1>
+    );
+}
---
+++ src/app/f1/f4/(...)about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function InterceptedAbout() {
+    return (
+        <h1>
+            (...) Intercepted About page
+        </h1>
+    );
+}
